6bedcbfdf1d82b9a4e8363c04a988e4e8126d48e
Michael Mathias <michaelscodingjourney@gmail.com>
2023-03-02 17:13:25 -0800

    Passed seven tests for objects lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Dominique",
+    streetAddress: "1234 Coding Drive",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const nEmployee = {...employee};
+    nEmployee[key] = value;
+    return nEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const nEmployee = {...employee};
+    delete nEmployee[key];
+    return nEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
